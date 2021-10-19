d7986755cbdcbe5a38d6c1cb7144bda04ba9f39b
vladimir-gaydamakin <86650413+vladimir-gaydamakin@users.noreply.github.com>
2021-10-19 14:07:31 +0300

    Update Main.java
    try try try

--- src/main/java/ru/dataart/academy/java/Main.java
+++ src/main/java/ru/dataart/academy/java/Main.java
@@ -5,8 +5,20 @@ public class Main {
         ReverseInteger reverseInteger = new ReverseInteger();
         try {
             System.out.println("Reverse integer works: " + reverseInteger.reverse(-456));
+        } catch (ReverseInteger.IntegerOutOfBoundsException e) {
+            System.out.println(e.getMessage());
+        }
+        try {
             System.out.println("Reverse integer works: " + reverseInteger.reverse(-456));
+        } catch (ReverseInteger.IntegerOutOfBoundsException e) {
+            System.out.println(e.getMessage());
+        }
+        try {
             System.out.println("Reverse integer works for big negative value: " + reverseInteger.reverse(-2147483648));
+        } catch (ReverseInteger.IntegerOutOfBoundsException e) {
+            System.out.println(e.getMessage());
+        }
+        try {
             System.out.println("Reverse integer works for big positive value: " + reverseInteger.reverse(-2147483647));
         } catch (ReverseInteger.IntegerOutOfBoundsException e) {
             System.out.println(e.getMessage());
